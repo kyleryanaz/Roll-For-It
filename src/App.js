cb39cdcbf97d0dc2855d48d48b7ecf1dbca156f6
Kyle <kyleryan480@gmail.com>
2017-10-24 22:20:00 -0700

    W3 Style, Dropdown, Button, and Grid

--- src/App.js
+++ src/App.js
@@ -8,6 +8,29 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Roll For It</h1>
         </header>
+        <div className="App-toolbar">
+          <div class="w3-row">
+            <div class="w3-col m6 w3-center">
+              <div class="w3-dropdown-hover">
+                <button class="w3-button w3-black">Hover Over Me!</button>
+                <div class="w3-dropdown-content w3-bar-block w3-border">
+                  <a href="#" class="w3-bar-item w3-button">
+                    Link 1
+                  </a>
+                  <a href="#" class="w3-bar-item w3-button">
+                    Link 2
+                  </a>
+                  <a href="#" class="w3-bar-item w3-button">
+                    Link 3
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="w3-col m6 w3-center">
+              <button class="w3-button w3-black">Button Button</button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
